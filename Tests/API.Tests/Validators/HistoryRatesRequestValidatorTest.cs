648a63d72560f351f0846a7802257d225a928ba5
muchachos <indijanero@gmail.com>
2019-07-13 22:44:30 +0200

    `

--- Tests/API.Tests/Validators/HistoryRatesRequestValidatorTest.cs
+++ Tests/API.Tests/Validators/HistoryRatesRequestValidatorTest.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using ExchangeRateGateway.API.Validators;
-using ExchangeRateGateway.Domain.Model;
 using FluentValidation.TestHelper;
 using Xunit;
 
